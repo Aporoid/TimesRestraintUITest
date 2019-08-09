cc1fb1c307a30b34495a63d5f1bba7cd204e54d7
Aporoid <Aporoid@hotmail.com>
2019-08-09 18:26:22 -0500

    Finishing up Inspection tables

--- TR-UI-test/Assets/Scripts/WeaponController.cs
+++ TR-UI-test/Assets/Scripts/WeaponController.cs
@@ -1,18 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WeaponController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField]
+	private Text weaponTitle;
+	[SerializeField]
+	private Text weaponLevel;
+	[SerializeField]
+	private Text weaponDescription;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	[SerializeField]
+	private Image Gun1;
+	[SerializeField]
+	private Image Gun2;
+
+	[SerializeField]
+	private Text statNumbers;
+
+	[SerializeField]
+	private Image perkImage;
+	[SerializeField]
+	private Text perkName;
+	[SerializeField]
+	private Text perkDesc;
+
+	public void RapidfireFrame()
+	{
+		weaponTitle.text = "Storm's-a-Brewing";
+		weaponLevel.text = "Level 9 Auto rifle";
+		weaponDescription.text = "Bring forth the rain.";
+		Gun2.enabled = false;
+		statNumbers.text = "42" + "\n" + "60" + "\n" + "45" + "\n" + "45" + "\n" + "720 RPM";
+		perkImage.enabled = false;
+		perkName.text = "";
+		perkDesc.text = "";
+	}
+
+	public void SinglefireFrame()
+	{
+		weaponTitle.text = "Arc Conductor";
+		weaponLevel.text = "Level 13 Precision rifle";
+		weaponDescription.text = "Bring forth the rain.";
+		Gun2.enabled = true;
+		statNumbers.text = "85" + "\n" + "80" + "\n" + "85" + "\n" + "10" + "\n" + "60 RPM";
+		perkImage.enabled = true;
+		perkName.text = "Shocking Twist";
+		perkDesc.text = "Headshot kills stun nearby enemies.";
+	}
 }
