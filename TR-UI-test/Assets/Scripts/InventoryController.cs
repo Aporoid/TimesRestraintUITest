040f44efd6aab2d2247104a85331178becfe9cfc
Aporoid <Aporoid@hotmail.com>
2019-08-09 17:22:08 -0500

    Adding weapon inspect screen

--- TR-UI-test/Assets/Scripts/InventoryController.cs
+++ TR-UI-test/Assets/Scripts/InventoryController.cs
@@ -13,12 +13,18 @@ public class InventoryController : MonoBehaviour
 	private Image PillsImage;
 	[SerializeField]
 	private Image EtherImage;
+	[SerializeField]
+	private Image ShotgunSupplyImage;
+	[SerializeField]
+	private Image RarityBackdrop;
 
 	//used to refresh the image every time the page is switched.
 	public void Refresh()
 	{
 		PillsImage.enabled = false;
 		EtherImage.enabled = false;
+		ShotgunSupplyImage.enabled = false;
+		RarityBackdrop.color = Color.black;
 		nameText.text = "";
 		DescriptionText.text = "";
 	}
@@ -44,22 +50,30 @@ public class InventoryController : MonoBehaviour
 
 	public void Restorative1()
 	{
+		RarityBackdrop.color = Color.green;
 		EtherImage.enabled = false;
 		PillsImage.enabled = true;
+		ShotgunSupplyImage.enabled = false;
 		nameText.text = "Pain pills";
 		DescriptionText.text = "Fast acting aspirin to ease pain." + "\n" + "\n" + "Restores 20% HP on use.";
 	}
 
 	public void Restorative2()
 	{
+		RarityBackdrop.color = Color.green;
 		PillsImage.enabled = false;
 		EtherImage.enabled = true;
+		ShotgunSupplyImage.enabled = false;
 		nameText.text = "Ancient Ether";
 		DescriptionText.text = "Nectar from the Guardians." + "\n" + "\n" + "Restores 30% Time Energy on use.";
 	}
 
 	public void Restorative3()
 	{
+		RarityBackdrop.color = Color.blue;
+		PillsImage.enabled = false;
+		EtherImage.enabled = false;
+		ShotgunSupplyImage.enabled = true;
 		nameText.text = "Shotgunner's supplies";
 		DescriptionText.text = "A crate of 000 Buckshot." + "\n" + "\n" + "Grants 16 shotgun shells on use.";
 	}
